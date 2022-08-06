fedaf13f565aaf43a4fa6a9c8be2ec09e87fe7ee
Crowdbotics <team@crowdbotics.com>
2022-08-06 23:36:25 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/weedbarbies_36642/MainActivity.java
@@ -0,0 +1,15 @@
+package com.weedbarbies_36642;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "weedbarbies_36642";
+  }
+}
